7abff29585b8128bb6c0823346a44d20886ec7cc
ntsokos <ntsokos@gmail.com>
2017-11-12 21:59:25 +0000

    observables work as well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { ITreeNode } from 'angular-tree-component/dist/defs/api';
 import { TreeComponent } from 'angular-tree-component';
 // tslint:disable-next-line:import-blacklist
 import { Observable } from 'rxjs';
+import { PACKAGE_ROOT_URL } from '@angular/core/src/application_tokens';
 
 interface MyNode {
   id: number;
@@ -29,11 +30,11 @@ export class AppComponent {
       const nodes = this.getChildNodes(node);
 
       return Observable.of(nodes)
-        .delay(2000)
+        .delay(500)
         .toPromise();
 
     }
-  }
+  };
 
   public getChildNodes(node: ITreeNode): MyNode[] {
     if (node.id === 1) {
@@ -45,17 +46,53 @@ export class AppComponent {
       return [
         { id: 5, name: 'child2.1', hasChildren: false },
         { id: 6, name: 'child2.2', hasChildren: true }
-      ]
+      ];
     } else if (node.id === 6) {
       return [
         { id: 7, name: 'subsub', hasChildren: false }
-      ]
+      ];
     }
 
   }
 
   expandTest(): void {
     // 4 6 7
+
+    // this.expandPathByUsingPromises();
+    // this.expandPathByUsingObservables().subscribe(() => {
+    //   console.log('finished');
+    // });
+    // this.expandPathByUsingObservables().subscribe(() => {
+    //   console.log('finished');
+    // });
+  }
+
+  private expandPathByUsingObservables(): Observable<boolean> {
+
+    const treeModel = this.tree.treeModel;
+    const node_root2 = treeModel.getNodeById(4);
+
+    const node_root_expand = Observable.fromPromise(node_root2.expand() as Promise<any>);
+
+    const retObs$ = node_root_expand.flatMap(() => {
+      const node_child2_2 = treeModel.getNodeById(6) as ITreeNode;
+      console.log(node_child2_2.data.name);
+
+      const node_child2_2_expand = Observable.fromPromise(node_child2_2.expand() as Promise<any>);
+      return node_child2_2_expand.flatMap(() => {
+
+        const node_subsub = treeModel.getNodeById(7) as ITreeNode;
+        console.log(node_subsub.data.name);
+
+        return Observable.of(true);
+      })
+
+    });
+
+    return retObs$;
+  }
+
+  private expandPathByUsingPromises(): void {
     const treeModel = this.tree.treeModel;
     const node_root2 = treeModel.getNodeById(4);
     const node_root_expand = node_root2.expand() as Promise<any>;
@@ -66,16 +103,11 @@ export class AppComponent {
       node_child2_2_expand.then(() => {
 
         const node_subsub = treeModel.getNodeById(7) as ITreeNode;
-        console.log(node_subsub);
+        console.log(node_subsub.data.name);
 
       });
 
-    })
-
-    // alert('testing expand');
+    });
   }
 
-  private expandPath(path: string[]): void {
-
-  }
 }
